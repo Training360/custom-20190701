0cf926bb0b245cda50bfccdc7ebaaae0369c7548
vicziani <viczian.istvan@gmail.com>
2019-07-02 11:03:10 +0200

    Teszteset

--- employeewebapp/src/test/java/training360/employeewebapp/EmployeewebappApplicationTests.java
+++ employeewebapp/src/test/java/training360/employeewebapp/EmployeewebappApplicationTests.java
@@ -2,15 +2,26 @@ package training360.employeewebapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EmployeewebappApplicationTests {
 
+	@Autowired
+	EmployeeService employeeService;
+
 	@Test
-	public void contextLoads() {
+	public void testSaveThenList() {
+		employeeService.createEmployee("Test User 1");
+
+		var names = employeeService.listEmployeeNames();
+
+		assertTrue(names.contains("Test User 1"));
 	}
 
 }
